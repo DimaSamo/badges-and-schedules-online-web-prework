d54a54f36ca71d345af83ad5da9cbf0dabccf354
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-06 19:16:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(names)
+  badges=[]
+  names.each do |name|
+    badges.pop("Hello, my name is#{name}.")
+  end
+  badges
+end
